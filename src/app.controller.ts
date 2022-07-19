189c21e402d9f8a3d009602235e62d1752ecc0e2
Tergel <tergel7006@gmail.com>
2022-07-19 10:54:37 +0800

    test: get method

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  // @Get()
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
+
+  @Get('users')
+  getData(): string {
+    return this.appService.getData();
+  }
+  
+  @Get('hello/:term')
+  public sampleRoute(@Param('term') term: string): string {
+    return `Hello, ${term}`;
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import data from '../../data.json';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  };
+  getData(): string {
+    // console.log(data);
+    // console.log('sdfs')
+    return 'Success!';
   }
 }
+
